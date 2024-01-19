4564426aef1a2900e833a34edd842356ffa19675
leticia.chijo <leticia.chijo@gmail.com>
2024-01-19 19:57:55 -0300

    Aula 03

---
+++ src/main/java/com/tastecamp/api/controllers/RecipeController.java
@@ -0,0 +1,43 @@
+package com.tastecamp.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipeController {
+
+    @GetMapping    
+    public String getRecipes() {
+        return "Lista de Receitas";
+    }
+
+    @GetMapping("/{id}")
+    public String getRecipeById(@PathVariable Long id) {
+        return "Essa é a receita " + id;
+    }
+
+    @PostMapping
+    public String createRecipe(@RequestBody String body) {
+        System.out.println(body);
+        return body;
+    }
+
+    @PutMapping("/{id}")
+    public String updateRecipe(@PathVariable Long id, @RequestBody String body) {
+        return body + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRecipe(@PathVariable Long id) {
+        return "Deletando item " + id;
+    }
+    
+}
